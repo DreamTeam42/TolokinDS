c2517533008cb57cd8b3f3aa554a67eb17140941
tolokindmitriy <tolo.dima@gmail.com>
2017-07-10 00:46:10 +0300

    Расширен список считываемых данных

--- avito_scra.py
+++ avito_scra.py
@@ -1,5 +1,5 @@
 import scrapy
-
+import json
 
 #class BashSpider(scrapy.Spider):
     # имя парсера
@@ -30,16 +30,32 @@ class AvitoSpider (scrapy.Spider):
     # имя парсера
     name = 'AvitoSpider'
 #список ссылок для парсинга
-    start_urls=['http://avito.ru/volgograd/kvartiry']
+    start_urls=['http://avito.ru/volgograd/kvartiry/prodam']
 
 #функция парсинга
     def parse(self, responce):
-        item=responce.css('title-info-title')
+        item=responce.css('div.item-view-content')
+
     #собираем данные в словарь
+
         yield {
-            'title': item.css('span.title-info-title-text::text').extract_first(),
-            'contactFace':item.css('div.seller-info-value::text').extract_first()
+            'title': item.css('div.item-view-left div.title-info_mode-with-favorite div.title-info-main h1.title-info-title span.title-info-title-text::text').extract(),
+            'seller':item.css('div.item-view-contacts.js-item-view-contacts div.item-view-seller-info div.seller-info.js-seller-info div.seller-info-prop.js-seller-info-prop_seller-name.seller-info-prop_layout-two-col div.seller-info-col div.seller-info-value div.seller-info-name a::text').extract(),
+            'adress':item.css('div.item-view-contacts.js-item-view-contacts div.item-view-seller-info div.seller-info.js-seller-info div.seller-info-prop div.seller-info-value::text').extract()[4:],
+            'number of rooms':item.css('div.item-view-main.js-item-view-main div.item-view-block div.item-params ul.item-params-list li.item-params-item::text').extract(),
+            'floor': item.css('div.item-view-main.js-item-view-main div.item-view-block div.item-params ul.item-params-list li.item-params-list-item::text').extract()[5:6],
+            'type':item.css('div.item-view-main.js-item-view-main div.item-view-block div.item-params ul.item-params-list li.item-params-list-item::text').extract()[7:8],
+            'total area':item.css('div.item-view-main.js-item-view-main div.item-view-block div.item-params ul.item-params-list li.item-params-list-item::text').extract()[9:10],
+            'kitchen area':item.css('div.item-view-main.js-item-view-main div.item-view-block div.item-params ul.item-params-list li.item-params-list-item::text').extract()[11:12],
+            'living space':item.css('div.item-view-main.js-item-view-main div.item-view-block div.item-params ul.item-params-list li.item-params-list-item::text').extract()[13:14],
+            'description':item.css('div.item-view-block div.item-description div.item-description-text p::text').extract(),
+            'lat':item.css('div.b-search-map.expanded.item-map-wrapper.js-item-map-wrapper::attr(data-map-lat)').extract(),
+            'lon': item.css('div.b-search-map.expanded.item-map-wrapper.js-item-map-wrapper::attr(data-map-lon)').extract(),
+            'photo-list':item.css('div.gallery-list-wrapper ul.gallery-list.js-gallery-list li.gallery-list-item.js-gallery-list-item span.gallery-list-item-link::attr(style)').extract()
              }
+
+
     #итерируемся по url страниц
-        for next_page in responce.css('div.item_table-description div.item_table-header h3.item-description-title a.item-description-title-link::attr(href)'):
-         yield responce.follow(next_page,self.parse)
+        for next_page in responce.css('a.pagination-page::attr(href)'):
+         for next_page in responce.css('div.item_table-description div.item_table-header h3.item-description-title a.item-description-title-link::attr(href)'):
+            yield responce.follow(next_page,self.parse)
